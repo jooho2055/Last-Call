9a4cac95cce5120589844b12a0f63a7ab53e9ef6
Pangwoo <gwangwoo25@gmail.com>
2023-09-11 11:48:50 -0700

    change structure

---
+++ application/server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = 5001;
+
+app.set("port", process.env.PORT || port);
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("/AboutUs", (req,res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
+app.get("/AboutUs", (req,res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "app listening at %{port}");
+});
