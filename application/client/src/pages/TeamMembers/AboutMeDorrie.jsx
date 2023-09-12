6c67dc77579cc4de8804c27556c12e254164f6ab
Luis Acuna <mendez97.luca@gmail.com>
2023-09-12 14:15:25 -0700

    my side

--- application/client/src/pages/TeamMembers/AboutMeDorrie.jsx
+++ application/client/src/pages/TeamMembers/AboutMeDorrie.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function AboutMeDorrie() {
-	return <div></div>;
+	return (
+		<div>
+			<h1>Hello mY name is joohosafdasfds and its working!! sdfyayay</h1>
+			<img
+				class='Jooho__avatar'
+				src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80'
+				alt="Jooho's profile"
+			/>
+		</div>
+	);
 }
