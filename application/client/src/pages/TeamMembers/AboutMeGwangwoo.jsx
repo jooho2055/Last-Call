cd86249978e50963fd5184ef40f44e3d5db96f5b
Pangwoo <gwangwoo25@gmail.com>
2023-09-12 13:51:54 -0700

    about me gwangwoo

--- application/client/src/pages/TeamMembers/AboutMeGwangwoo.jsx
+++ application/client/src/pages/TeamMembers/AboutMeGwangwoo.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import '../../App.css';
+import profile from '../../images/profiles/Gwangwoo_Profile.jpeg';
 
-export default function AboutMeGwangwoo() {
-	return <div></div>;
+export default function AboutMeJooho() {
+	return (
+		<div className='aboutme__container'>
+			<h1 className='aboutme__jooho'>Hello, my name is Gwangwoo</h1>
+			<p className='aboutme__major'>Major: Computer Science, Senior </p>
+			<p className='aboutme__role'>Role: Back-end Master</p>
+
+			<div className='aboutme__middle'>
+				<div className='aboutme__skills'>
+					<p className='aboutme__skill'>Skills: HTML, CSS, JavaScript, Java, Python, React</p>
+				</div>
+				<img className='aboutme__avatar' src={profile} alt="Jooho's profile" />
+			</div>
+
+			<p className='aboutme__main'>
+				I am really excited to take Software Engineering class with outstanding team members. It is
+				an honor to be able to participate in the project as a front end.
+			</p>
+		</div>
+	);
 }
