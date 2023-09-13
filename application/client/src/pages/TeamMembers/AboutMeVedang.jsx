dbdf8ee1dd1b4fb2a3b4b5e39425aedf49988a50
vedangsakxena <vedang.sakxena.friends@gmail.com>
2023-09-12 17:38:35 -0700

    intro

--- application/client/src/pages/TeamMembers/AboutMeVedang.jsx
+++ application/client/src/pages/TeamMembers/AboutMeVedang.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import myImage from '../../images/profiles/Vedang_profile.jpg';
 
 export default function AboutMeVedang() {
-	
 	return (
 		<div className="about-me">
-			<h1>Hello, I'm Luis!</h1>
-			<p>I am currrently a student in Computer Science that is going to be graduate this 
-				fall and is currently woring as a Korean server while also being able to get as much experience
-				in my carreer as possible. My hobbies will be from powerlifting to hiking becuase
-				I always believed that a healthy mind comes from a healthy body, while also enjoy reading
-				most of my free time.
+			<h1>Vedang Sakxena</h1>
+			<p>
+				A Graduate Student at SFSU, currently enrolled in Electrical and Computer Engineering.
+				In this team, my role is of the Scrum Master.
+				My hobbies include reading, writing and travelling. I also like to play sports like Cricket, football & basketball. 
 			</p>
-			
+			<img
+				className='vedang-avatar'
+				src={myImage}
+				width={300} 
+				height={300}
+				alt="vedang's profile"
+			/>
 		</div>
 	);
 }
+
