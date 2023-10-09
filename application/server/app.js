57c940f34f433f0d30ced145c3bccdf38e750194
Pangwoo <gwangwoo25@gmail.com>
2023-10-09 00:31:21 -0700

    app connect to  db

--- application/server/app.js
+++ application/server/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-
+var db = require('./conf/database');
 const port = 5001;
 
 app.set("port", process.env.PORT || port);
@@ -21,6 +20,12 @@ app.get("/AboutUs/:name", (req,res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
+app.get('/allMembers', async (req, res) => {
+  const query = `SELECT * FROM member `;
+  var [rows, fields] = await db.query(query, []);
+  res.send(rows);
+});
+
 app.listen(app.get("port"), () => {
-    console.log(app.get("port"), "app listening at %{port}");
+    console.log(app.get("port"), `app listening at ${port}`);
 });
