739bc1dbd2f6240fee98054046b0c128a721a35a
“Leslie <lesliemora12@gmail.com>
2023-10-10 12:32:07 -0700

    Nav

--- application/client/src/components/Navbar.jsx
+++ application/client/src/components/Navbar.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import iconProfile from './User_Profile.png';
+
 
 export default function Navbar() {
 	return (
 		<header className='header'>
 			<nav className='header__nav'>
 				<ul className='header__menu'>
+				<li className='header__menu__item'>
+						<Link to='/AboutUs'>LAST CALL</Link>
+					</li>
 					<li className='header__menu__item'>
 						<Link to='/'>Home</Link>
 					</li>
 					<li className='header__menu__item'>
 						<Link to='/AboutUs'>About Us</Link>
 					</li>
+					<li className='header__menu__item'>
+						<Link to='/AboutUs'>Current Reservation</Link>
+					</li>
+					<li className='header__menu__item'>
+						<Link to='/AboutUs'>Order History</Link>
+					</li>
+					<li className="header__menu__item1 profile-icon">
+            		<img src={iconProfile} alt="icon"  />
+         		 </li>
 				</ul>
 			</nav>
 		</header>
 	);
-}
+} 
