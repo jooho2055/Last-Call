161d2e9f122e01a2a90626a10f0285af3949baee
Pangwoo <gwangwoo25@gmail.com>
2023-10-10 13:39:41 -0700

    dummpy

--- application/server/app.js
+++ application/server/app.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get(`/search?search=${searchValue}`, async(req, res)=>{
+  const query = `SELECT * FROM member `;
+  console.log(req.query.search);
+  console.log(req.query.search == null);
+  if(req.query.search == null){
+    var [rows, fields] = await db.query(query, []);
+    res.status(200).json(rows);
+  }else{
+    var [rows, fields] = await db.query(query, []);
+    res.status(200).json(rows);
+  }
+  
+
+  res.status(200).json(rows[0]);
+})
+
 app.get("/AboutUs", (req,res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
