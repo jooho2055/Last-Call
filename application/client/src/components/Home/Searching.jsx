26de2ee667545b230b30bc14891bf29b380d8741
Dongrui Shen <dongrui.shen@gmail.com>
2023-10-10 13:57:31 -0700

    update code

--- application/client/src/components/Home/Searching.jsx
+++ application/client/src/components/Home/Searching.jsx
@@ -1,30 +1,22 @@
 import './Search.css';
-import { useRef } from 'react';
+import { useState } from 'react';
 import React from 'react';
-import axios from 'axios';
-const Search=()=>{
-  const inputRef = useRef(null);
-  async function submit(e){
-    e.preventDefault()
-    try{
-      await axios.post("http://localhost:3000/sendData",{
-        inputRef
-      })
-    }
-    catch(e){
-      console.log(e)
-    }
-  }
-  function handleSubmit () {
-    console.log(inputRef.current.value);
+
+function Search() {
+  const[inputValue, setValue] = useState("");
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    alert(`The value you entered was: ${inputValue}`)
+
   }
   return(
     <div className='search-container'>
-     
-     <input type="text" className='searchbox' placeholder='Search...' ref={inputRef}/>
-     <button type="submit" className='searchbutton' onClick={handleSubmit}>
+     <form onSubmit={handleSubmit}>
+     <input type="text" className='searchbox' placeholder='Search...' value={inputValue} onChange={(e) => setValue(e.target.value)}/>
+     <button type="submit" className='searchbutton'>
      <img className='searchpicture' src='https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg' alt="searchingico"/>
     </button>
+    </form>
      </div>   
 
   )
