2ba0fa2ba3df3369958037025419951cd369fe41
“Leslie <lesliemora12@gmail.com>
2023-10-10 22:59:50 -0700

    No Dropdown

--- application/client/src/components/Header/Navbar.jsx
+++ application/client/src/components/Header/Navbar.jsx
@@ -7,28 +7,40 @@ import iconProfile from '../../images/profiles/User_Profile.png';
 export default function Navbar() {
 	return (
 		<header className='header'>
-			<nav className='header__nav'>
-				<ul className='header__menu'>
-				<li className='header__menu__item'>
-						<Link to='/AboutUs'>LAST CALL</Link>
-					</li>
-					<li className='header__menu__item'>
-						<Link to='/'>Home</Link>
-					</li>
-					<li className='header__menu__item'>
-						<Link to='/AboutUs'>About Us</Link>
-					</li>
-					<li className='header__menu__item'>
-						<Link to='/AboutUs'>Current Reservation</Link>
-					</li>
-					<li className='header__menu__item'>
-						<Link to='/AboutUs'>Order History</Link>
-					</li>
-					<li className="header__menu__item1 profile-icon">
-            		<img src={iconProfile} alt="icon"  />
-         		 </li>
-				</ul>
-			</nav>
-		</header>
-	);
-} 
+      <nav className='header__nav'>
+        <ul className='header__menu'>
+          <div className='header__menu__item'>
+            <li>
+              <Link>LAST CALL</Link>
+            </li>
+          </div>
+          <div className='header__menu__item'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+          </div>
+          <div className='header__menu__item'>
+            <li>
+              <Link to='/AboutUs'>About Us</Link>
+            </li>
+          </div>
+          <div className='header__menu__item'>
+            <li>
+              <Link to='/AboutUs'>Current Reservation</Link>
+            </li>
+          </div>
+          <div className='header__menu__item'>
+            <li>
+              <Link to='/AboutUs'>Order History</Link>
+            </li>
+          </div>
+          <div className="header__menu__item profile-icon">
+            <li>
+              <img src={iconProfile} alt="icon" />
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+}
