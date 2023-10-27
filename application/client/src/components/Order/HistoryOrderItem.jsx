182453ba2a9a8c5b66b94694315e7dec5553696f
Dorrie Shen <dongrui.shen@gmail.com>
2023-10-26 18:02:49 -0700

    adjust position

--- application/client/src/components/Order/HistoryOrderItem.jsx
+++ application/client/src/components/Order/HistoryOrderItem.jsx
@@ -2,17 +2,32 @@ import React from 'react';
 
 export default function HistoryOrderItem(props) {
     return (
-        <div>
+        <div className='border border-slate-600 relative h-64 w-80 flex flex-col'>
             <p>Date: {props.date}   {props.time}</p>
-            <div>
+            <div className='relative bottom-0 left-0'>
                 <p>Restaurant: {props.restaurant_name}</p>
                 <p>Address: {props.address}</p>
                 <p>Menu List:</p>
-               {props.foods.map((food)=>(
-                   <li>{food.name}- {food.quantity}- {food.price}</li>
-               ))}
+                <table className='border-spacing-4'>
+                <tr>
+                    <th>Food Name</th>
+                    <th className='p-2'>Quantity</th>
+                    <th className='p-2'>Price</th>
+                </tr>
+            {props.foods.map((food)=>(
+                    <tr>
+                        <td>{food.name}</td>
+                        <td className='p-2'>{food.quantity}</td>
+                        <td className='p-2'>{food.price}</td>
+                    </tr>
+                
+            ))}
+            </table>
             </div>
-            <div><p>Origial Price: {props.original_p}   Actual paid: {props.actual_p}</p></div>
+            <div className='absolute bottom-0 right-0'>
+                <p>Original Price: {props.original_p}</p>
+                <p>Actual paid: {props.actual_p}</p>
+            </div>
          <br />   
         </div>
     );
--- application/client/src/pages/Order.jsx
+++ application/client/src/pages/Order.jsx
@@ -26,6 +26,8 @@ const inputsForcurrenttest =[
 		date: "May, 23, 2023",
 		time:"9 pm",
 		status: "pending",
+		original_p:"$6",
+		actual_p:"$5",
 	},
 	{
 		restaurant_name: "Test2",
@@ -48,6 +50,8 @@ const inputsForcurrenttest =[
 		date: "May, 24, 2023",
 		time:"9 pm",
 		status: "pending",
+		original_p:"$6",
+		actual_p:"$5",
 	},
 	{
 		restaurant_name: "Test3",
@@ -70,15 +74,17 @@ const inputsForcurrenttest =[
 		date: "May, 25, 2023",
 		time:"9 pm",
 		status: "pending",
+		original_p:"$6",
+		actual_p:"$5",
 	}
 ]
 
 
 export default function Order() {
-	return <div className='h-full flex flex-col justify-center items-center bg-white'>
+	return <div className='h-full w-full flex flex-col justify-center items-center bg-white gap-4'>
 		<h1>Order</h1>
 		<h3>Current Order</h3>
-		<div className='flex flex-row'>
+		<div className='flex flex-row border-spacing-3 gap-4'>
 		{inputsForcurrenttest.map((input)=>(
 			<CurrentOrderItem 
 			restaurant_name={input.restaurant_name}
@@ -92,14 +98,15 @@ export default function Order() {
 		</div>
 		<br />
 		<h3>Order History</h3>
-		<div className='grid grid-cols-2'>
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
 			{inputsForcurrenttest.map((input)=>(
 				<HistoryOrderItem
 				restaurant_name={input.restaurant_name}
 			    address={input.address}
 			    foods ={input.foods}
 			    date={input.date}
-			    price={input.price}
+			    original_p={input.original_p}
+				actual_p={input.actual_p}
 			    time={input.time}/>
 			))}
 
