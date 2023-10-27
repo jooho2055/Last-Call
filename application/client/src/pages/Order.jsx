dce9e755234cd0c0893dbabdc9518560ebe1bfbe
Dorrie Shen <dongrui.shen@gmail.com>
2023-10-27 12:14:16 -0700

    update

--- application/client/src/pages/Order.jsx
+++ application/client/src/pages/Order.jsx
@@ -82,9 +82,11 @@ const inputsForcurrenttest =[
 
 export default function Order() {
 	return <div className='h-full w-full flex flex-col justify-center items-center bg-white gap-4'>
+		<div className='bg-slate-900 text-white text-center w-full h-10'>	
 		<h1>Order</h1>
+		</div>
 		<h3>Current Order</h3>
-		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4'>
 		{inputsForcurrenttest.map((input)=>(
 			<CurrentOrderItem 
 			restaurant_name={input.restaurant_name}
