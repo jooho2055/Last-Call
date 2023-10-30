34e5967a8c8b6998ba38a820c733ecd4279d9c77
Dongrui Shen <dongrui.shen@gmail.com>
2023-10-30 10:18:25 -0700

    update

--- application/client/src/components/Order/currentOrderItem.jsx
+++ application/client/src/components/Order/currentOrderItem.jsx
@@ -9,7 +9,7 @@ export default function CurrentOrderItem({currentorderInfo}) {
             <p>Address: {address}</p>
             <p>Date: {date}</p>
             <p>Pick up by: {time}</p>
-            <p>Status: {status}</p>
+            <p>Status: {status === 0 ? "pending" : "Done"}</p>
             <p>Menu List:</p>
             <div className='border border-collapse border-spacing-4'>
                 <div className="grid grid-cols-3 border-b bg-gray-100 font-semibold">
