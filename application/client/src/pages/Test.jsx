649625d6187aa8ae92aabc8cc74e56a78f4e4e5b
Pangwoo <gwangwoo25@gmail.com>
2023-10-30 16:05:01 -0700

    rest info

--- application/client/src/pages/Test.jsx
+++ application/client/src/pages/Test.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+
+import React, {useState, useEffect}from 'react';
 import MenuInfo from '../components/MenuInfo';
 
 export default function Test() {
+	const [menus, setMenus] = useState({});
+	async function fetchData(){
+		const res = await fetch(`http://13.52.182.209/restaurants/menu/list/1`,{
+			method: 'GET',
+			headers: {
+				"content-Type": 'application/json'
+			}, 
+		})
+		const data = res.json()
+		console.log(data)
+		console.log(res)
+	}
+	useEffect( () => {
+		fetchData();
+	}, []);
 	return (
 		<div>
 			<MenuInfo/>
