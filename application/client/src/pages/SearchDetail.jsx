ae3f2ff6e67ad2e3208fe87cff9909f9f69036a4
jooho2055 <jooho2055@gmail.com>
2023-11-01 01:43:58 -0700

    quick bug fix

--- application/client/src/pages/SearchDetail.jsx
+++ application/client/src/pages/SearchDetail.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import RestaurantList from '../components/RestaurantList';
 
+// export default function SearchDetail() {
+// 	const { isLoading, error, data: restaurants } = useQuery({ queryKey: ['restaurants'] });
+// 	return (
+// 		<div className='grid grid-cols-3 auto-rows-[minmax(14rem,auto)] p-6 gap-6'>
+// 			{restaurants.map((restaurant, index) => (
+// 				<RestaurantList key={index} restaurantInfo={restaurant} /> // must use restaurant unique id as key in the future
+// 			))}
+// 		</div>
+// 	);
+// }
+
 export default function SearchDetail() {
-	const { isLoading, error, data: restaurants } = useQuery({ queryKey: ['restaurants'] });
-	return (
-		<div className='grid grid-cols-3 auto-rows-[minmax(14rem,auto)] p-6 gap-6'>
-			{restaurants.map((restaurant, index) => (
-				<RestaurantList key={index} restaurantInfo={restaurant} /> // must use restaurant unique id as key in the future
-			))}
-		</div>
-	);
+	return <div>This is SearchDetail page!!</div>;
 }
