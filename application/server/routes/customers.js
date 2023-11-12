cd332ff8df5b5bc385e74cd0cf9d9145dfc23c53
Pangwoo <gwangwoo25@gmail.com>
2023-11-11 22:35:33 -0800

    fix menu error

--- application/server/routes/customers.js
+++ application/server/routes/customers.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../conf/database');
-// const cors = require('cors')
 const path = require("path");
 const {isLoggedIn, isCustomers, isMyPage} = require('../middleware/auth')
-
-// let corsOptions = {
-//   origin: ['http://localhost:3000', 'http://localhost:5001'],
-//   credentials: true
-// }
-
-// router.use(cors(corsOptions))
 
 router.get(`/search`, async(req, res)=>{
     const query = `SELECT * FROM restaurants `;
@@ -134,9 +126,9 @@ DEBUG_CART_ADD = {
 router.post('/order/cart/add', /*isLoggedIn, isCustomers,*/ async function(req,res){
     const {customerId, menuId, restaurantId} = req.body
     // const {customerId, menuId, restaurantId} = DEBUG_CART_ADD;
-    if(req.session.user.userId !== customerId){
-        return res.status(400).json({message: "wrong access!"})
-    }
+    // if(req.session.user.userId !== customerId){
+    //     return res.status(400).json({message: "wrong access!"})
+    // }
 
     try{
         var [result, _ ] = await db.execute(`SELECT * FROM menus WHERE id = ?;`,[menuId])
