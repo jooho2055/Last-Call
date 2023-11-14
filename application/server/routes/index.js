840193e4b0d8433fd3312256e3e6bd5ae0defbf8
Pangwoo <gwangwoo25@gmail.com>
2023-11-14 00:54:40 -0800

    edit search api

--- application/server/routes/index.js
+++ application/server/routes/index.js
@@ -78,19 +78,15 @@ const customerStorage = multer.diskStorage({
 
 const upload = multer({ storage: customerStorage });
 
-// router.get("/", function(req, res){
-//     res.sendFile(path.join(__dirname, "/../../client/build/index.html"));
-//   });
-
 router.get(`/search`, async(req, res)=>{
 const {search} = req.query;
 console.log("this is search value: ", search)
 try{
     if(search == null){
-        var [rows, fields] = await db.execute(`SELECT id, name, cuisine, status, img_path, concat(address, ' ', city, ' ', state, ' ', zipcode) as address FROM restaurants WHERE status = 'open';`);
+        var [rows, fields] = await db.execute(`SELECT id, name, cuisine, status, img_path, concat(address, ', ', city, ', ', state, ', ', zipcode) as address FROM restaurants WHERE status = 'open';`);
         res.status(200).json(rows);
     }else{
-        var [rows, fields] = await db.query(`SELECT id, name, cuisine, status, img_path, concat(address, ' ', city, ' ', state, ' ', zipcode) as address, concat_ws(' ', name, cuisine, status) as haystack FROM restaurants having haystack like ?;`, [`%${search}%`]);
+        var [rows, fields] = await db.query(`SELECT id, name, cuisine, status, img_path, concat(address, ', ', city, ', ', state, ', ', zipcode) as address, concat_ws(' ', name, cuisine, status) as haystack FROM restaurants having haystack like ?;`, [`%${search}%`]);
         res.status(200).json(rows);
     }
 }catch(error){
@@ -100,6 +96,6 @@ try{
 
 router.post(`/test`,upload.single('file'), (req, res) => {
     res.send('File uploaded successfully.');
-  });
+});
 
 module.exports = router;
