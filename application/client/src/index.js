a5ee12bb02ccf1d175fe4744d6189f457d744488
“Leslie <lesliemora12@gmail.com>
2023-11-14 23:43:13 -0800

    check

--- application/client/src/index.js
+++ application/client/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Provider } from "react-redux";
-import store from './redux/store'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
 
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
@@ -19,7 +18,8 @@ root.render(
         <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </React.StrictMode>
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
--- application/client/src/pages/CustomerProfile.jsx
+++ application/client/src/pages/CustomerProfile.jsx
@@ -1,67 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
-import {SET_USER_DATA} from '../redux/userActions2';
+import { SET_USER_DATA } from '../redux/userActions2';
 import ProfileInput from '../components/ProfileInput';
 import { inputsUserProfile } from '../utils/cusProfile';
+import { useQuery, QueryClient,QueryClientProvider  } from 'react-query';
 
-
-
+const fetchUserProfile = async (username) => {
+  const userProfileUrl = `http://13.52.182.209/customers/getUserProfile/${username}`;
+  const response = await axios.get(userProfileUrl);
+  return response.data;
+};
 
 export default function CustomerProfile() {
-    const [inputValues, setInputValues] = useState({
-        username: null,
-        fname: null,
-        lname: null,
-        email: null,
-        pwd: null,
-        cpwd: null,
-        phone: null,
-        bio: null,
-      });
-    
     const user = useSelector((state) => state.user);
     const username = user.username;
-
     const dispatch = useDispatch();
 
-    const [successMessage, setSuccessMessage] = useState('');
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const queryClient = new QueryClient();
 
-    useEffect(() => {
-        const userProfileUrl = `http://13.52.182.209/customers/getUserProfile/username=${username}`;
-      
-        axios.get(userProfileUrl)
-          .then((response) => {
-            const userData = response.data;
-            console.log('userData:', userData);
-            setInputValues((prevValues) => userData || prevValues);
-            setIsLoading(false);
-          })
-          .catch((error) => {
-            console.error('Error fetching user profile:', error);
-            setError(`An error occurred while fetching user profile: ${error.message}`);
-            setIsLoading(false);
-          });
-      }, [username]);
-      
-      
+  const { data: userProfile, error: fetchError, isLoading } = useQuery(
+    ['userProfile', username], 
+    () => fetchUserProfile(username)
+    );
+
+  const [inputValues, setInputValues] = useState({
+    username: userProfile?.username || '',
+    fname: userProfile?.fname || '',
+    lname: userProfile?.lname || '',
+    email: userProfile?.email || '',
+    pwd: userProfile?.pwd || '',
+    cpwd: userProfile?.cpwd || '',
+    phone: userProfile?.phone || '',
+    bio: userProfile?.bio || '',
+  });
+
+  useEffect(() => {
+    if (userProfile) {
+      setInputValues(userProfile);
+    }
+  }, [userProfile]);
+
+  const [validity, setValidity] = useState({
+    username: true,
+    fname: true,
+    lname: true,
+    email: true,
+    pwd: true,
+    cpwd: true,
+    phone: true,
+    bio: true,
+  });
+
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMsg, setErrorMsg] = useState(null);
 
 
-    const [validity, setValidity] = useState({
-		username: true,
-		fname: true,
-		lname: true,
-		email: true,
-		pwd: true,
-		cpwd: true,
-		phone: true,
-        bio: true,
-	});
-    const isSubmitDisabled =
-        !Object.values(validity).every((isValid) => isValid) ||
-        !Object.values(inputValues).every((value) => value);
+  const isSubmitDisabled =
+    !Object.values(validity).every((isValid) => isValid) || 
+    !Object.values(inputValues).every((value) => value);
 
     const validateInput = (name, value) => {
         let isValid = true;
@@ -102,81 +99,80 @@ export default function CustomerProfile() {
 
     function checkpassword(pwd, cpwd) {
         if (pwd && inputValues.cpwd) {
-            if (pwd !== cpwd) {
-                return false;
-            }
+          if (pwd !== cpwd) {
+            return false;
+          }
         }
         return true;
-    }
+      }
 
-    const handleChange = (event) => {
+      const handleChange = (event) => {
         const { name, value } = event.target;
         setInputValues({ ...inputValues, [name]: value });
         validateInput(name, value);
-    };
-
-    const handleSubmit = (event) => {
+      };
+    
+      const handleSubmit = (event) => {
         event.preventDefault();
         const editProfileUrl = 'http://13.52.182.209/customers/edit';
-
-        axios.post(editProfileUrl, inputValues)
-         .then((response) => {
-            const userData = response.data;
-            dispatch({ type: SET_USER_DATA, payload: userData });
-            toggleEditMode();
+    
+        axios
+          .post(editProfileUrl, inputValues)
+          .then((response) => {
+            const updatedUserData = response.data;
+            dispatch({ type: SET_USER_DATA, payload: updatedUserData });
             setSuccessMessage('Profile updated successfully!');
             setTimeout(() => {
-                setSuccessMessage('');
-              }, 5000);
+              setSuccessMessage('');
+            }, 5000);
           })
           .catch((error) => {
-            console.error('Error fetching user profile:', error);
-            setError('An error occurred while updating your profile. Please try again later.');
-            if (error.response) {
-                console.error('Response data:', error.response.data);
-                console.error('Response status:', error.response.status);
-                console.error('Response headers:', error.response.headers);
-              }
+            console.error('Error updating user profile:', error);
+            setErrorMsg('An error occurred while updating your profile. Please try again later.');
           });
-    };
+      };
+    
 
-    const [editMode, setEditMode] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
-    const toggleEditMode = () => {
-        setEditMode(!editMode);
-        };
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
 
   return (
-    <div className='container mx-auto px-4 py-8'>
-            <form onSubmit={handleSubmit} className='bg-slate-100 p-8 rounded-lg shadow-md max-w-md mx-auto'>
-            <h1 className='text-xl mb-4 text-center font-semibold text-slate-950'>User Profile</h1>
-            {isLoading ? (
-                <div className='text-center'>Loading user profile data...</div>
-                ) : (
-                <>
+    <QueryClientProvider client={new queryClient()}> 
+    <div className="container mx-auto px-4 py-8">
+      <form onSubmit={handleSubmit} className="bg-slate-100 p-8 rounded-lg shadow-md max-w-md mx-auto">
+        <h1 className="text-xl mb-4 text-center font-semibold text-slate-950">User Profile</h1>
+        {isLoading ? (
+          <div className="text-center">Loading user profile data...</div>
+        ) : (
+          <>
             {!isLoading && inputValues !== null && inputsUserProfile.map((input) => (
-                <ProfileInput
+              <ProfileInput
                 key={input.id}
                 {...input}
                 value={inputValues[input.name] || ''}
                 onChange={handleChange}
                 isValid={validity[input.name]}
                 disabled={!editMode}
-                />
-                ))}
-                <div className="mt-4 flex justify-center">
-                <button className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-full" disabled={isSubmitDisabled}>
-                    Save
-                </button>
-                <button type='button' className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" onClick={toggleEditMode}>
-                    {editMode ? 'Cancel' : 'Edit'}
-                </button>
-                </div>
-                </>
-                )}
-                {successMessage && <div className='text-green-600 text-center'>{successMessage}</div>}
-             {error && <div className='text-red-600 text-center'>{error}</div>}      
-        </form>
+              />
+            ))}
+            <div className="mt-4 flex justify-center">
+              <button className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded-full" disabled={isSubmitDisabled}>
+                Save
+              </button>
+              <button type="button" className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" onClick={toggleEditMode}>
+                {editMode ? 'Cancel' : 'Edit'}
+              </button>
+            </div>
+          </>
+        )}
+        {successMessage && <div className="text-green-600 text-center">{successMessage}</div>}
+        {errorMsg && <div className="text-red-600 text-center">{errorMsg}</div>}
+        {fetchError && <div className="text-red-600 text-center">{fetchError.message}</div>}
+      </form>
     </div>
+    </QueryClientProvider>
   );
 }
