3e62a7a02cbbc76cda5aa969903024147c71bd9e
“Leslie <lesliemora12@gmail.com>
2023-11-28 16:23:17 -0800

    edit of info is fetched

--- application/client/src/pages/CustomerProfile.jsx
+++ application/client/src/pages/CustomerProfile.jsx
@@ -7,27 +7,6 @@ import { inputsUserProfile } from '../utils/cusProfile';
 
 const BASE_URL = 'http://13.52.182.209';
 
-const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/customers/getUserProfile/${username}`);
-  
-    const contentType = response.headers['content-type'];
-
-    if (contentType && contentType.includes('application/json')) {
-      const userData = response.data;
-      console.log('User Profile Data:', userData);
-    } else if (contentType && contentType.includes('text/html')) {
-      const htmlData = response.data;
-      console.log('HTML Response:', htmlData);
-    } else {
-      console.error('Unsupported Content Type:', contentType);
-    }
-
-  } catch (error) {
-    console.error('Error fetching user profile data:', error.message);
-  }
-};
-
 export default function CustomerProfile() {
   const dispatch = useDispatch();
 
@@ -59,12 +38,7 @@ export default function CustomerProfile() {
   const [htmlResponse, setHtmlResponse] = useState(null);
 
   useEffect(() => {
-    // Trying to fetch my own username that I creted
-    // username: qwaszx
-    // password: qwaszx1!
-    const username = 'qwaszx'; 
-
-    const fetchUserData = async (username) => {
+    const fetchUserData = async () => {
       try {
         const response = await axios.get(`${BASE_URL}/customers/getUserProfile/?username=qwaszx`);
   
@@ -91,8 +65,9 @@ export default function CustomerProfile() {
       }
     };
 
-    fetchUserData(username);
+    fetchUserData();
   }, []); 
+
   const isSubmitDisabled =
     !Object.values(validity).every((isValid) => isValid) ||
     !Object.values(inputValues).every((value) => value);
@@ -103,45 +78,49 @@ export default function CustomerProfile() {
       case 'username':
         isValid = /^[A-Za-z0-9]{5,16}$/.test(value);
         break;
-        case 'fname':
-            case 'lname':
-              isValid = /^[A-Za-z]+$/.test(value);
-              break;
-            case 'pwd':
-              isValid =
-                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/.test(
-                  value
-                ) && checkpassword(value, inputValues.cpwd);
-              break;
-            case 'cpwd':
-              isValid = value === inputValues.pwd;
-              break;
-            case 'email':
-              isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-              break;
-            case 'phone':
-              isValid = /^\d{10,15}$/.test(value);
-              break;
-            case 'bio':
-              isValid = /^[A-Za-z0-9\s]{0,100}$/.test(value);
-              break;
-             default:
-              isValid = false;
-          }
+      case 'fname':
+      case 'lname':
+        isValid = /^[A-Za-z]+$/.test(value);
+        break;
+      case 'pwd':
+        isValid =
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/.test(
+            value
+          ) && checkpassword(value, inputValues.cpwd);
+        break;
+      case 'cpwd':
+        isValid = value === inputValues.pwd;
+        break;
+      case 'email':
+        isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        break;
+      case 'phone':
+        isValid = /^\d{10,15}$/.test(value);
+        break;
+      case 'bio':
+        isValid = /^[A-Za-z0-9\s]{0,100}$/.test(value);
+        break;
+      default:
+        isValid = false;
+    }
     setValidity({ ...validity, [name]: isValid });
   };
+
   function checkpassword(pwd, cpwd) {
     if (pwd && inputValues.cpwd) {
       if (pwd !== cpwd) {
         return false;
-      }}
+      }
+    }
     return true;
   }
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setInputValues({ ...inputValues, [name]: value });
     validateInput(name, value);
   };
+
   const handleSave = (e) => {
     e.preventDefault(); 
     console.log('Request Payload:', inputValues); // Log the payload
@@ -164,9 +143,11 @@ export default function CustomerProfile() {
         );
       });
   };
+
   const toggleEditMode = () => {
     setEditMode(!editMode);
   };
+
   return (
     <div className="container mx-auto mt-10">
       <form
@@ -176,47 +157,75 @@ export default function CustomerProfile() {
         }}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2 mx-auto">
         <h1 className="text-2xl mb-4 text-center font-semibold text-gray-700">
-         User Profile</h1>
-       {editMode ? (
-         <>
-           {inputsUserProfile.map((input) => (
-             <ProfileInput
-               key={input.id}
-               {...input}
-               value={inputValues[input.name] || ''}
-               onChange={handleChange}
-               isValid={validity[input.name]}
-               disabled={!editMode}
-             />))}
-           <div className="mt-4 flex justify-center">
-             <button
-               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-               disabled={isSubmitDisabled}
-               onClick={handleSave}>Save</button>
-             <button
-               type="button"
-               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full ml-2"
-               onClick={toggleEditMode}>Cancel</button></div>
-         </>
-       ) : (
-         <>
-           {inputsUserProfile.map((input) => (
-             <div key={input.id} className="mb-4"><label
-                 htmlFor={input.name}
-                 className="block text-gray-700 font-bold mb-2">
-                 {input.label}
-               </label>
-               <div className="text-gray-900">{inputValues[input.name]}</div>
-             </div>))}
-           <div className="mt-4 flex justify-center">
-             <button
-               type="button"
-               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-               onClick={toggleEditMode}>Edit</button>
-           </div>
-         </>
-       )}
-       {successMessage && (<div className="text-green-600 text-center mt-4">{successMessage}</div>)}
-       {errorMsg && (<div className="text-red-600 text-center mt-4">{errorMsg}</div>)}
-         {htmlResponse && (<div className="text-red-600 text-center mt-4">{htmlResponse}</div>)}
-     </form></div>);}
+          User Profile
+        </h1>
+        {editMode ? (
+          <>
+            {inputsUserProfile.map((input) => (
+              <ProfileInput
+                key={input.id}
+                {...input}
+                value={inputValues[input.name] || ''}
+                onChange={handleChange}
+                isValid={validity[input.name]}
+                disabled={!editMode}
+              />
+            ))}
+            <div className="mt-4 flex justify-center">
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                disabled={isSubmitDisabled}
+                onClick={handleSave}
+              >
+                Save
+              </button>
+              <button
+                type="button"
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full ml-2"
+                onClick={toggleEditMode}
+              >
+                Cancel
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            {inputsUserProfile.map((input) => (
+              <div key={input.id} className="mb-4">
+                <label
+                  htmlFor={input.name}
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  {input.label}
+                </label>
+                <div className="text-gray-900">
+                  {inputValues[input.name]}
+                </div>
+              </div>
+            ))}
+            <div className="mt-4 flex justify-center">
+              <button
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={toggleEditMode}
+              >
+                Edit
+              </button>
+            </div>
+          </>
+        )}
+        {successMessage && (
+          <div className="text-green-600 text-center mt-4">
+            {successMessage}
+          </div>
+        )}
+        {errorMsg && (
+          <div className="text-red-600 text-center mt-4">{errorMsg}</div>
+        )}
+        {htmlResponse && (
+          <div className="text-red-600 text-center mt-4">{htmlResponse}</div>
+        )}
+      </form>
+    </div>
+  );
+}
