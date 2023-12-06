bfd042796f265204fdf87465fe54196e605823e5
jooho2055 <jooho2055@gmail.com>
2023-12-06 07:46:33 -0800

    quick fix

--- application/client/src/pages/Customer/CustomerOrder.jsx
+++ application/client/src/pages/Customer/CustomerOrder.jsx
@@ -28,38 +28,40 @@ export default function CustomerOrder() {
 			<div className='mt-14 mb-14'>
 				<span className='text-3xl'>Current Order</span>
 				<div className='grid grid-cols-3 pt-20 gap-12'>
-					{orders.map((order, index) => {
-						const restaurant = restaurants[index];
+					{currentOrder &&
+						currentOrder.orders?.map((order, index) => {
+							const restaurant = currentOrder.restaurants[index];
 
-						return (
-							<OrderCard
-								key={index}
-								className='w-96 h-96 bg-[#f7e8dc] shadow-[6.0px_9.0px_9.0px_rgba(0,0,0,0.30)] rounded-lg font-medium text-lg mx-auto flex flex-col justify-between '
-								restaurant={restaurant}
-								order={order}
-								isCurrentOrder={true}
-							></OrderCard>
-						);
-					})}
+							return (
+								<OrderCard
+									key={index}
+									className='w-96 h-96 bg-[#f7e8dc] shadow-[6.0px_9.0px_9.0px_rgba(0,0,0,0.30)] rounded-lg font-medium text-lg mx-auto flex flex-col justify-between '
+									restaurant={restaurant}
+									order={order}
+									isCurrentOrder={true}
+								></OrderCard>
+							);
+						})}
 				</div>
 			</div>
 
 			<div className='mb-14'>
 				<span className='text-3xl'>Order History</span>
 				<div className='grid grid-cols-3 mt-20 gap-12'>
-					{historyOrders.map((order, index) => {
-						const restaurant = historyRestaurants[index];
+					{orderHistory &&
+						orderHistory.orders?.map((order, index) => {
+							const restaurant = orderHistory.restaurants[index];
 
-						return (
-							<OrderCard
-								key={index}
-								className='w-[21rem] h-80 bg-[#e6edf5] shadow-[6.0px_9.0px_9.0px_rgba(0,0,0,0.30)] rounded-lg font-medium text-lg mx-auto flex flex-col justify-between'
-								restaurant={restaurant}
-								order={order}
-								isCurrentOrder={false}
-							></OrderCard>
-						);
-					})}
+							return (
+								<OrderCard
+									key={index}
+									className='w-[21rem] h-80 bg-[#e6edf5] shadow-[6.0px_9.0px_9.0px_rgba(0,0,0,0.30)] rounded-lg font-medium text-lg mx-auto flex flex-col justify-between'
+									restaurant={restaurant}
+									order={order}
+									isCurrentOrder={false}
+								></OrderCard>
+							);
+						})}
 				</div>
 			</div>
 		</div>
